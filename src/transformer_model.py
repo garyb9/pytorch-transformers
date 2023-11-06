1d084d2845b1931ce158243a15feed84ab713326
garyb9 <gberkinfand@gmail.com>
2023-11-06 20:24:36 +0200

    adding feed forward block

--- src/transformer_model.py
+++ src/transformer_model.py
@@ -66,3 +66,16 @@ class LayerNormalization(nn.Module):
         mean = x.mean(dim=-1, keepdim=True)
         std = x.std(dim=-1, keepdim=True)
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+
+class FeedForwardBlock(nn.Module):
+
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)  # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_model, d_ff)  # W2 and B2
+
+    def forward(self, x: torch.Tensor):
+        # (Batch, Seq_len, d_model) --> (Batch, Seq_len, d_ff) --> (Batch, Seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
