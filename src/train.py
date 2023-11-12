32842064bbca193c3ea19e013b3609a3ecc0332a
garyb9 <gberkinfand@gmail.com>
2023-11-12 11:31:03 +0200

    adding training libraries

---
+++ src/train.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
