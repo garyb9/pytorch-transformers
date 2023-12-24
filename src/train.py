9d76ad9f2dbb20ea89817a53e68b89f2ed689a36
garyb9 <gberkinfand@gmail.com>
2023-12-24 17:13:49 +0200

    closing train code

--- src/train.py
+++ src/train.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader, random_split
 
+from dataset import BilingualDataset, causal_mask
+from model import build_transformer
+
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
@@ -45,3 +48,34 @@ def get_dataset(config):
     val_dataset_size = len(dataset_raw) - train_dataset_size
     train_dataset_raw, val_dataset_raw = random_split(
         dataset_raw, [train_dataset_size, val_dataset_size])
+
+    train_dataset = BilingualDataset(train_dataset_raw, tokenizer_src,
+                                     tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_dataset = BilingualDataset(val_dataset_raw, tokenizer_src, tokenizer_tgt,
+                                   config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in dataset_raw:
+        src_ids = tokenizer_src.encode(
+            item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(
+            item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Max length of source sentence: {max_len_src}')
+    print(f'Max length of target sentence: {max_len_tgt}')
+
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(vocab_src_len, vocab_tgt_len,
+                              config['seq_len'], config['seq_len'], config['d_model'])
+    return model
